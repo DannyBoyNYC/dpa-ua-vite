295bb4a3af9a902ccd886bbd1e5e04ebfebb01d2
DannyBoyNYC <daniel.deverell@gmail.com>
2024-09-08 18:52:44 -0400

    configuration

---
+++ sandbox/main.js
@@ -0,0 +1,16 @@
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+    </a>
+    <h1>Testing UA Parser</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
